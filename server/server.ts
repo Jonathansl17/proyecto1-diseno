0d537b373d587ecfa2d0fa6222e620fa4eef892d
Jonathansl17 <jonathansancho5087@gmail.com>
2025-11-26 21:56:20 -0600

    Configure CORS middleware to allow requests from any origin

--- server/server.ts
+++ server/server.ts
@@ -58,7 +58,10 @@ const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',  // Permitir solicitudes de cualquier origen
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
